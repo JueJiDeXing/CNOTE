2856241a79b2a9326c24aa729df7fcd6527ae832
JueJiDeXing <3408470465@qq.com>
2023-12-16 19:46:13 +0800

    C语言数据结构

--- Algorithm/Sort/ShellSort.c
+++ Algorithm/Sort/ShellSort.c
@@ -13,7 +13,7 @@
  * 每组内插入排序,排序后gap变小,直到gap为1完成排序(每次除2)
  */
 void shellSort(int *a, int len) {
-    printf("开始排序:\n");
+    //printf("开始排序:\n");
     for (int gap = len >> 1; gap >= 1; gap = gap >> 1) {//使用位运算代替除法
         //插入排序,但是不比较相邻元素,而是相隔gap的元素
         for (int low = gap; low < len; low++) {//low=gap,第一个元素视为已排;low++,排下一组
@@ -27,7 +27,7 @@ void shellSort(int *a, int len) {
                 a[i + gap] = t;
             }
         }
-        printArr(a, len);
+        //printArr(a, len);
     }
 }
 
